20ea5123fc24ff8be1fd955f7b4e6b10d1b06776
Ankan Mandal <112895542+Coderankan59@users.noreply.github.com>
2023-09-22 14:57:43 +0530

    This file is added

---
+++ Addition.cpp
@@ -0,0 +1,15 @@
+// Addition of the two integer numbers
+#include<stdio.h>
+int main()
+{
+	int x,y,addition;
+	printf("Addition of the two integer numbers\n");
+	printf("Enter the first integer number: ");
+	scanf("%d",&x);
+	printf("Enter the second integer number: ");
+	scanf("%d",&y);
+	addition=x+y;
+	printf("The Addition of the %d and %d is: %d",x,y,addition);
+	return 0;
+}
+
